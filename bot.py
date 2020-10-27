65f72676f0038f8f3245fdb621f01fcb2cb9aaff
Gilad Sher <triplesher6@gmail.com>
2020-10-28 00:26:20 +0200

    fixes and changes after first stream

--- bot.py
+++ bot.py
@@ -8,34 +8,31 @@ MAX_ALLOWED_TIME = 3
 MIN_ALLOWED_TIME = 0
 POSSIBLE_TIMED_COMMANDS = {
     "!fwd1": "w",
+    "!fwd": "w",
+    "!up": "w",
     "!fwd2": "w+q",
     "!right1": "d",
+    "!right": "d",
     "!right2": "d+f",
     "!left1": "a",
+    "!left": "a",
     "!left2": "a+e",
     "!back1": "s",
+    "!back": "s",
+    "!down": "s",
     "!back2": "s+x",
-    "!cast": "alt"
+    "!turnr": "p",
+    "!turnl": "o",
+    "!viewup": "b+w",
+    "!viewdown": "b+s",
+    "!skip": "tab",
+    "!cast": "b"
 }
 POSSIBLE_NON_TIMED_COMMANDS = {
     "!jump": "space",
     "!map": "r",
-    "!potion": "ctrl"
-}
-
-HELP_MESSAGES = {
-    "!fwd1": "go forwards using 1 key-timed",
-    "!fwd2": "go forwards using 2 keys-timed",
-    "!right1": "go right using 1 key-timed",
-    "!right2": "go right using 2 keys-timed",
-    "!left1": "go left using 1 key-timed",
-    "!left2": "go left using 2 keys-timed",
-    "!back1": "go back using 1 key-timed",
-    "!back2": "go back using 2 keys-timed",
-    "!cast": "holds cast - timed",
-    "!jump": "jump",
-    "!map": "open the map",
-    "!potion": "use a potion"
+    "!potion": "ctrl",
+    "!esc": "esc"
 }
 
 # set up the bot
@@ -48,15 +45,14 @@ bot = commands.Bot(
 )
 
 
-async def send_keyboard_event(ctx, command_name, timing=DEFAULT_PRESS_TIME):
+def send_keyboard_event(ctx, command_name, timing=DEFAULT_PRESS_TIME):
     if command_name in POSSIBLE_TIMED_COMMANDS:
         keyboard.press(POSSIBLE_TIMED_COMMANDS[command_name])
-        time.sleep(timing)
-        keyboard.release(POSSIBLE_TIMED_COMMANDS[command_name])
+        keyboard.call_later(lambda: keyboard.release(
+            POSSIBLE_TIMED_COMMANDS[command_name]), delay=timing)
+
     elif command_name in POSSIBLE_NON_TIMED_COMMANDS:
         keyboard.send(POSSIBLE_NON_TIMED_COMMANDS[command_name])
-    else:
-        await ctx.channel.send(f"Whoops @{ctx.author.name}! Looks like that's not a valid command!\n Try to use !help for more information.")
 
 
 async def send_error_message(ctx):
@@ -67,27 +63,20 @@ async def handle_message(ctx):
     split_message_string = ctx.content.split(' ')
     command_name = split_message_string[0]
     try:
-        timing = split_message_string[1]
+        timing_string = split_message_string[1]
         try:
-            timing = float(timing)
-            if timing > MAX_ALLOWED_TIME or timing <= MIN_ALLOWED_TIME:
+            timing_float = float(timing_string)
+            if timing_float > MAX_ALLOWED_TIME or timing_float <= MIN_ALLOWED_TIME:
                 raise Exception('Number not in allowed range')
             else:
-                # use the keyboard command synchronasly to prevent malicious keyboard inputs
-                await send_keyboard_event(ctx, command_name, timing)
-        except Exception:
+                send_keyboard_event(ctx, command_name, timing=timing_float)
+                return
+        except ValueError:
             await send_error_message(ctx)
+            return
     except Exception:
-        await send_keyboard_event(ctx, command_name)
-
-
-async def send_help_message(ctx):
-    full_string = ''
-    message_intro = f'@{ctx.author.name} Possible commands (timed commands default to 0.5 seconds and accept a value between 0-3):'
-    full_string = full_string + message_intro
-    for command in HELP_MESSAGES:
-        full_string += f'{command} : {HELP_MESSAGES[command]}, '
-    await ctx.channel.send(full_string[:-1])
+        send_keyboard_event(ctx, command_name)
+        return
 
 
 @bot.event
@@ -103,27 +92,24 @@ async def event_message(ctx):
     'Runs every time a message is sent in chat.'
 
     # make sure the bot ignores itself and the streamer
-    if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
-        return
+    # if ctx.author.name.lower() == os.environ['BOT_NICK'].lower():
+    #     return
 
     await bot.handle_commands(ctx)
-
-    if 'hello' in ctx.content.lower():
-        await ctx.channel.send(f"Hi, @{ctx.author.name}!")
-
-
-@bot.command(name="help")
-async def help(ctx):
-    await send_help_message(ctx)
-
-
-@bot.command(name='test')
-async def test(ctx):
-    keyboard.press('alt')
 
 
 @bot.command(name="fwd1")
 async def fwd1(ctx):
+    await handle_message(ctx)
+
+
+@bot.command(name="fwd")
+async def fwd(ctx):
+    await handle_message(ctx)
+
+
+@bot.command(name="up")
+async def up(ctx):
     await handle_message(ctx)
 
 
@@ -137,6 +123,11 @@ async def right1(ctx):
     await handle_message(ctx)
 
 
+@bot.command(name="right")
+async def right(ctx):
+    await handle_message(ctx)
+
+
 @bot.command(name="right2")
 async def right2(ctx):
     await handle_message(ctx)
@@ -147,6 +138,11 @@ async def left1(ctx):
     await handle_message(ctx)
 
 
+@bot.command(name="left")
+async def left(ctx):
+    await handle_message(ctx)
+
+
 @bot.command(name="left2")
 async def left2(ctx):
     await handle_message(ctx)
@@ -154,6 +150,16 @@ async def left2(ctx):
 
 @bot.command(name="back1")
 async def back1(ctx):
+    await handle_message(ctx)
+
+
+@bot.command(name="back")
+async def back(ctx):
+    await handle_message(ctx)
+
+
+@bot.command(name="down")
+async def down(ctx):
     await handle_message(ctx)
 
 
@@ -179,6 +185,36 @@ async def jump(ctx):
 
 @bot.command(name="cast")
 async def cast(ctx):
+    await handle_message(ctx)
+
+
+@bot.command(name="esc")
+async def esc(ctx):
+    await handle_message(ctx)
+
+
+@bot.command(name="turnr")
+async def turnr(ctx):
+    await handle_message(ctx)
+
+
+@bot.command(name="turnl")
+async def turnl(ctx):
+    await handle_message(ctx)
+
+
+@bot.command(name="skip")
+async def skip(ctx):
+    await handle_message(ctx)
+
+
+@bot.command(name="viewup")
+async def viewup(ctx):
+    await handle_message(ctx)
+
+
+@bot.command(name="viewdown")
+async def viewdown(ctx):
     await handle_message(ctx)
 
 if __name__ == "__main__":
